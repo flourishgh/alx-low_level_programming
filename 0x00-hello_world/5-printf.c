ff1c05d106b21c718b16b9d3cdf81b5f0852cbd6
=Kusi Samuel <=info.flourish.com>
2023-02-16 22:00:02 +0000

    Task 4 Hello Puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+  * main - Print message to the screen
+  *Return: Always return 0
+  */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art");
+return (0);
+}
