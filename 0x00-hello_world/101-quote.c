30ce3958be898e3825111325ea369493100f4dc9
=Kusi Samuel <=info.flourish.com>
2023-02-16 22:20:47 +0000

    Task 4 Hello Puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-/**                                                   
-* main - prints a string and standard error
- * Return: 1                                          
-*/
-int main(void)                                       {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
-return (1);
+/**
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+return (1);
 }
