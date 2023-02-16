37d9bdaeb1b177639c00f25c84618a2e221ffc5d
=Kusi Samuel <=info.flourish.com>
2023-02-16 22:06:16 +0000

    Task 4 Hello Puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /**
   * main - Print message to the screen
-  *Return: Always return 0
+  *Return: Always return 0 (success)
   */
-int main(void)
+int main()
 {
 printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
