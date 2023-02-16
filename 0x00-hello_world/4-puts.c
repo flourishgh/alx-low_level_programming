11dd37dd1e584934804c32964de3946e85fc059c
=Kusi Samuel <=info.flourish.com>
2023-02-16 21:36:47 +0000

    Task 4 Hello Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,13 @@
 /**
  * main - Program starting point 
  *
- * Descriptio : Print Text to the user
+ * Description : Print Text to the user
  *
  * Return : Always 0 ( Success )
 */
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts ("\"Programming is like building a multilingual puzzle");
 return (0);
 }
