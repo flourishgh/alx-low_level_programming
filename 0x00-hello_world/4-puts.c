c242223b15a1f18b94d0399ad86919fef4c25e20
=Kusi Samuel <=info.flourish.com>
2023-02-16 21:25:34 +0000

    Task 4 Hello Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/* 
- *Print programming is like building a multiligual puzzle
-*/
+
+/*Print programming is like building a multiligual puzzle*/
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
