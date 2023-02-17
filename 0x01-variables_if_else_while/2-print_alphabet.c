ce38d5d697eb1de8900e87ac5ffa09153f11619b
=Kusi Samuel <=info.flourish.com>
2023-02-17 22:41:16 +0000

    Hello

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,10 +5,32 @@
  */
 int main(void)
 {
-for(int i=97; i < 124; i++)
-{
-char alpha = i;
-putchar(alpha);
-}
+putchar(97);
+putchar(98);
+putchar(99);
+putchar(100);
+putchar(101);
+putchar(102);
+putchar(103);
+putchar(104);
+putchar(105);
+putchar(106);
+putchar(107);
+putchar(108);
+putchar(109);
+putchar(110);
+putchar(111);
+putchar(112);
+putchar(113);
+putchar(114);
+putchar(115);
+putchar(116);
+putchar(117);
+putchar(118);
+putchar(119);
+putchar(120);
+putchar(121);
+putchar(122);
+putchar(123);
 return (0);
 }
