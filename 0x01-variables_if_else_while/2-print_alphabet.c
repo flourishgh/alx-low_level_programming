034d2604ebd3b7b2a1b67813802e0f3affa10e7b
=Kusi Samuel <=info.flourish.com>
2023-02-17 22:33:40 +0000

    Hello

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Print alphabets
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+for(int i=97; i < 124; i++)
+{
+char alpha = i;
+putchar(alpha);
+}
+return (0);
+}
