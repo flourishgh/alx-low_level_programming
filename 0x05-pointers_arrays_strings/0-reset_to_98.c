2b93bd2319fcfe0ef2d8bf9848a644d445e7fa98
Kusi Samuel <info.kusi.gh@gmail.com>
2023-02-28 01:37:53 +0000

    a

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
- * main- check code
+ *reset_to_98 - function that takes a pointer to an int as parameter and
  *
  * @n: This is the entry
  */
 void reset_to_98(int *n)
 {
- *n=98;
+*n=98;
 }
-
