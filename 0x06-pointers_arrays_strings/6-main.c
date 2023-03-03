0744b8cb00097478d3432905583a466b2ccdc351
Kusi Samuel <info.kusi.gh@gmail.com>
2023-03-03 03:23:21 +0000

    a

--- 0x06-pointers_arrays_strings/6-main.c
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -8,11 +8,11 @@
  */
 int main(void)
 {
-    char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
-    char *p;
+    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+    char *ptr;
 
     p = cap_string(s);
-    printf("%s", p);
-    printf("%s", s);
+    printf("%s", ptr);
+    printf("%s", str);
     return (0);
 }
