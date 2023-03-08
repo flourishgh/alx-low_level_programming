104a1a3fb00c6865c5cf3e4706b9c949289c817a
Kusi Samuel <edu.kovakhill@gmail.com>
2023-03-08 21:12:51 +0000

    a

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,7 +2,7 @@
 #include "main.h"
 /**
  * _print_rev_recursion - Print String In Reverse Form Using Recursion
- * @str - The String to be printed in reverse form
+ * @str; The String to be printed in reverse form
  * Return: Always void (success)
  */
 void _print_rev_recursion(char *str)
