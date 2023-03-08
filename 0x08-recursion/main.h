b2383c745fe5f33c15dab9a5ce49264574956985
Kusi Samuel <edu.kovakhill@gmail.com>
2023-03-08 21:48:10 +0000

    Hello

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,23 @@
-void _putrecursion(char *s);
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: Kusi Samuel
+ * Desc: Header file containing prototypes for all alx functions
+ *       used in the 0x07-recursion directory.
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
-int _strlen_recursion(char *str);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
+#endif
+
