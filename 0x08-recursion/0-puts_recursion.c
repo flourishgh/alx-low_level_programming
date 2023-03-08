bb65f9bb2bf86f5d61cbaaf096ae21eb8dcd4e04
Kusi Samuel <edu.kovakhill@gmail.com>
2023-03-08 20:51:54 +0000

    0x08

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,18 +3,17 @@
 /**
  * _puts_recursion - Prints a string, followed by a new line.
  * @str: The string to be printed.
- * 
  * Return: Always void (Success)
  */
 void _puts_recursion(char* str)
 {
-   if(*str == '\0')
-    {
-      putchar('\n');
-      return;
-    }
-  putchar(*str);
-  _puts_recursion(str+1);
+	if (*str == '\0')
+         {
+      		putchar('\n');
+		return;
+         }
+putchar(*str);
+_puts_recursion(str + 1);
 }
 
 
