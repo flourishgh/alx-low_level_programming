73ab64b29eba89da6d65c24a86c33f5c13f438ac
Kusi Samuel <edu.kovakhill@gmail.com>
2023-03-13 17:00:55 +0000

    kusi samuel cypher

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 /**
- * main - Print the name of the file
- * Return: Always return the file name
+ * main - Prints the program name, followed by a new line.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always o.
  */
 int main(int argc,char *argv[])
 {
