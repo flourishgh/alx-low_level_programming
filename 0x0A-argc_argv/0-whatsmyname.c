23e4fa371a3eb69000a69f3b7ebb7ac198f67bcc
Kusi Samuel <edu.kovakhill@gmail.com>
2023-03-13 16:57:53 +0000

    kusi samuel cypher

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Print the name of the file
+ * Return: Always return the file name
+ */
+int main(int argc,char *argv[])
+{
+ (void)argc;
+ printf("%s",argv[0]);
+ return 0;
+}
