de56e27bc322f90f629f9147969edb2de12ad5a7
Kusi Samuel <edu.kovakhill@gmail.com>
2023-03-16 18:33:44 +0000

    Hello

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,2 +1,19 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/*
+ * malloc_checked - Allocate memory using malloc
+ * Description: Allocate memory using malloc.
+ * @b: The number of bytes to be allocated.
+ *
+ * Return: A pointer to allocated memory.
+ */
 
+void *malloc_checked(usigned int b)
+{
+	void *mem = malloc(b);
+	
+	if(mem == NULL)
+		exit(98);
+	
+	return (mem);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * Creator: Kusi Samuel
+ * Desc: Header file containing prototypes for all functions
+ */
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+
+#endif
