dbd048faf284f360e788def632dcebf661c239c7
Kusi Samuel <info.flourishgh.com>
2023-03-22 01:56:38 +0000

    First Commit Of 0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print the filename of the program
+ * Return: Always (EXIT_SUCCESS),
+ * Description: print the filename of the program
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (EXIT_SUCCESS);
+}
