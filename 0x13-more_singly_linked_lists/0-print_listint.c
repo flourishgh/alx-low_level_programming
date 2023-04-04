3be9e8547803d30c3e4940c308c51c59b347b839
Kusi Samuel <info.flourishgh.com>
2023-04-04 20:29:57 +0000

    cypher

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * print_listint - a function that prints all the elements of a listint_t list.
+ * @h: listint_t pointer
+ * Return: Return number of nodes
+ * Description: a function that prints all the elements of a listint_t list.
+ */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *tmp = (listint_t *) malloc(sizeof(listint_t));
+	int numberOfNodes = 0;
+
+	tmp = h;
+
+	while (tmp != NULL)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+		numberOfNodes++;
+	}
+	return (numberOfNodes);
+}
