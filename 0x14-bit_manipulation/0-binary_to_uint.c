4a158318ac6fc3c9a0cac10e922bb4c67422edb1
Kusi Samuel <info.flourishgh.com>
2023-04-10 18:55:18 +0000

    changed unction that converts a binary number to an unsigned int using bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,21 +6,30 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
-	int len = strlen(b);
-	int power = len - 1;
-	int i;
+	unsigned int result = 0;
+	int i = 0;
 
 	if (b == NULL)
 		return (0);
-	for (i = 0; i < len; i++)
+
+	while (b[i] != '\0')
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[i] == '0')
+		{
+			result <<= 1;
+		}
+		else if (b[i] == '1')
+		{
+			result <<= 1;
+			result |= 1;
+		}
+		else
+		{
 			return (0);
-		num +=(unsigned int) (b[i] - '0') * custom_pow(2, power);
-		power--;
+		}
+		i++;
 	}
-	return (num);
+	return (result);
 }
 /**
  * custom_pow - Return the power of a number
